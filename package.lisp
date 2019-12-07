b4ad731dab0e664dbaa75445972ffa0ecc128379
Isoraķatheð Zorethan <isoraqathedh.zorethan@gmail.com>
2019-12-07 12:58:26 +0800

    Publish some symbols.

--- package.lisp
+++ package.lisp
@@ -1,4 +1,9 @@
 ;;;; package.lisp
 
 (defpackage #:info.isoraqathedh.finite-state-machine
-  (:use #:cl))
+  (:use #:cl)
+  (:export #:finite-state-machine
+           #:state #:statep #:acceptingp
+
+           #:next-state #:next-state!
+           #:previous-state #:previous-state!))
